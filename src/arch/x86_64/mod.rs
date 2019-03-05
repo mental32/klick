64efaad30803caeacf37e259faa9443dc87b6218
mental <m3nta1@yahoo.com>
2019-03-05 02:50:46 +0000

    Add double call protection, enable nxe and wp bits and store mboot2 info

--- src/arch/x86_64/mod.rs
+++ src/arch/x86_64/mod.rs
@@ -1,24 +1,49 @@
-pub mod macros;
-
-pub mod interrupts;
-
 #[macro_use]
 pub mod vga;
 
+pub mod macros;
+pub mod interrupts;
+
+use x86_64::{
+    registers::{
+        model_specific::Efer,
+        control::{
+            Cr0,
+            Cr0Flags
+        }
+    },
+
+    instructions::interrupts::without_interrupts
+};
+
 use vga::Character;
 
-pub fn init() -> Result<(), &'static str> {
+use crate::kflagset;
+
+pub fn init(multiboot_addr: usize) -> Result<(), &'static str> {
+    kflagset!("Attempted to initialize arch::x86_64 twice!");
 
     // Clear the screen
     printfill!(Character::as_default_whitespace());
 
-    // Reload GDT
+    println!("[ OK ] Begin initialization...");
 
-    // Load IDT
+    without_interrupts(|| {
+        // Setup the Multiboot1 struct
+        let multiboot = unsafe { multiboot2::load(multiboot_addr) };
 
-    // Load allocator
+        unsafe {
+            // Enable NXE bit
+            Efer::write_raw(Efer::read_raw() | 1 << 11);
 
-    // Initialize hardware
+            // Enable write protect bit
+            Cr0::update(|flags| {
+                flags.toggle(Cr0Flags::WRITE_PROTECT);
+            })
+        }
+    });
+
+    println!("[ OK ] Completed initialization!");
 
     Ok(())
 }
